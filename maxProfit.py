d5c3a6fd8448eba2af586638cb7628d3b01e0db8
Visha242629 <101812736+Visha242629@users.noreply.github.com>
2026-03-19 21:10:37 +0530

    123. Best Time to Buy and Sell Stock III

--- maxProfit.py
+++ maxProfit.py
@@ -1,7 +1,21 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i - 1]:
-                profit += prices[i] - prices[i - 1]
-        return profit
+    def maxProfit(prices):
+        if not prices:
+            return 0
+        
+        buy1 = float('inf')
+        sell1 = 0
+        buy2 = float('inf')
+        sell2 = 0
+        
+        for price in prices:
+            # First transaction: standard "Buy Low, Sell High"
+            buy1 = min(buy1, price)
+            sell1 = max(sell1, price - buy1)
+            
+            # Second transaction: Reinvest the profit from sell1
+            # buy2 is the 'effective' price of the second stock
+            buy2 = min(buy2, price - sell1)
+            sell2 = max(sell2, price - buy2)
+            
+        return sell2
