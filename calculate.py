64360de96fd096f58bbdae9eb13aa87675259fed
Visha242629 <101812736+Visha242629@users.noreply.github.com>
2026-04-04 19:29:33 +0530

    Calculator 2nd

---
+++ calculate.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        sign = '+'
+        
+        for i in range(len(s)):
+            char = s[i]
+            
+            if char.isdigit():
+                num = num * 10 + int(char)
+            
+            if char in '+-*/' or i == len(s) - 1:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop() * num)
+                elif sign == '/':
+                    stack.append(int(stack.pop() / num))  # truncate toward 0
+                
+                sign = char
+                num = 0
+        
+        return sum(stack)
